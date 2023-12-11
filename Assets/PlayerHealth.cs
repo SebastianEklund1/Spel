3c750bbacdb25604ca758a86f444f59341855701
Ermias070309 <ermias.tsegaye@elev.lbs.se>
2023-12-11 16:02:32 +0100

    new
    new

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    int maxHealth = 3;
+
+    int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void Hurt(int amount)
+    {
+
+        health -= amount;
+        if (health <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        //TODO: Game over?
+    }
+}
